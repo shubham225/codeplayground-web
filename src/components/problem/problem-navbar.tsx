b1f9246ba733e18f61854cdeacccfa7270e3c7ea
Shubham Shinde <shubhamshinde225@gmail.com>
2025-01-24 15:02:21 +0530

    Minor UI changes

--- src/components/problem/problem-navbar.tsx
+++ src/components/problem/problem-navbar.tsx
@@ -3,5 +3,13 @@ import React from "react";
 type Props = {};
 
 export default function ProblemNavbar({}: Props) {
-  return <div>Navbar</div>;
+  return (
+    <div className="flex justify-center items-center border rounded-md px-4 gap-2">
+      <p className="text-accent-foreground font-bold">00</p>
+      <p className="text-muted-foreground">:</p>
+      <p className="text-accent-foreground font-bold">29</p>
+      <p className="text-muted-foreground">:</p>
+      <p className="text-accent-foreground font-bold">59</p>
+    </div>
+  );
 }
