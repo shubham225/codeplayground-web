722e8977b7090abe9f5a02a042501dd7d5a16fcd
Shubham Shinde <shubhamshinde225@gmail.com>
2025-06-09 21:14:22 +0530

    minor changes

--- src/app/(root)/settings/page.tsx
+++ src/app/(root)/settings/page.tsx
@@ -1,9 +1,7 @@
-import React from 'react'
+import React from "react";
 
-type Props = {}
+type Props = {};
 
 export default function page({}: Props) {
-  return (
-    <div>Settings Page</div>
-  )
+  return <div>Settings Page</div>;
 }
